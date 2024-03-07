7662dbefc2acf27e566e440b1c5504b8ab4f8e70
espressowizard <hajimedevlife@gmail.com>
2024-03-07 12:25:08 +0700

    init: first push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    transpilePackages: ["three"],
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            config.externals.push('sharp')
+        }
+
+        config.module.rules.push({
+            test: /\.(glsl|vs|fs|vert|frag)$/,
+            exclude: /node_modules/,
+            use: ['raw-loader', 'glslify-loader']
+        })
+
+        return config;
+    }
+};
 
 export default nextConfig;
---
+++ three-js.d.ts
@@ -0,0 +1,8 @@
+declare module "*.glsl" {
+  const file: string;
+  export default file;
+}
+
+declare module JSX {
+  interface IntrinsicElements {}
+}
